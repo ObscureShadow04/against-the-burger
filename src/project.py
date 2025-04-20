92d99ca87b8291997931fb1c498a4070676d8875
Ivan Vazquez <Ivanvqz404@gmail.com>
2025-04-20 01:39:20 -0500

    Successfully implemented new Player and GiantKillerSpaceRobot object classes that inherit from the newly written Character object class. Unlike the old ones, these new objects support animated sprites.

--- src/project.py
+++ src/project.py
@@ -59,7 +59,7 @@ class AnimatedSprite(Sprite):
     def set_framerate(self, fps=12):
         self.image_replace_time = 1.0 / fps
     
-    def update(self, dt=0):
+    def update(self, dt=0, pos=(0, 0)):
         self.time_since_last_replace += dt
 
         if self.time_since_last_replace >= self.image_replace_time:
@@ -70,6 +70,8 @@ class AnimatedSprite(Sprite):
             self.image_index = 0
         
         self.image = self.images[self.image_index]
+
+        self.draw_rect.center = pos
 
 class Character():
     def __init__(self, sprite_details=('', 3, 12), pos=(0, 0), lim=(0, DISPLAY_HEIGHT), hb=(20, 20), hp=5, s=200, cdt=1.0):
@@ -87,40 +89,157 @@ class Character():
         self.speed = s
 
         self.time_since_last_shoot = 0
-        self.cooldown_time = cdt
+        self.shoot_cooldown_time = cdt
     
+    def position_vector(self):
+        return (self.pos_x, self.pos_y)
+
     def _update_hitbox_rect_position(self, new_rect):
         self.pos_y = new_rect.center[1]
-        self.hitbox_rect.center = (self.pos_x, self.pos_y)
-        self.hitbox_rect.center = (self.pos_x, self.pos_y)
+        self.hitbox_rect.center = self.position_vector()
 
     def _manage_excessive_hitpoints(self):
         if self.hitpoints > self.max_hitpoints:
             self.hitpoints = self.max_hitpoints
 
-    def _manage_shoot_cooldown(self, dt=0):
-        if self.time_since_last_shoot <= self.cooldown_time:
+    def _manage_shoot_cooldown(self, dt=0, cooldown_time=0):
+        if self.time_since_last_shoot <= cooldown_time:
             self.time_since_last_shoot += dt
     
-    def _calculate_projected_position(self, dt=0, direction=0):
-        projected_y = self.pos_y + ((self.speed * direction) * dt)
+    def _calculate_projected_position(self, dt=0, direction=0, speed=0):
+        projected_y = self.pos_y + ((speed * direction) * dt)
         projected_hitbox_rect = self.hitbox_rect.copy()
         projected_hitbox_rect.center = (self.pos_x, projected_y)
         return projected_hitbox_rect
+    
+    def can_shoot(self):
+        if self.time_since_last_shoot >= self.shoot_cooldown_time:
+            return True
+        else:
+            return False
 
     def update(self, dt=0, direction=0):
         self._manage_excessive_hitpoints
+        self._manage_shoot_cooldown(dt, self.shoot_cooldown_time)
         
-        projected_hitbox_rect = self._calculate_projected_position(dt, direction)
+        projected_hitbox_rect = self._calculate_projected_position(dt, direction, self.speed)
         if not projected_hitbox_rect.top <= self.y_range[0] and not projected_hitbox_rect.bottom >= self.y_range[1]:
+            self.pos_y = projected_hitbox_rect.center[1]
             self._update_hitbox_rect_position(projected_hitbox_rect)
-
-        self._manage_shoot_cooldown(dt)
         
-        self.sprite.update(dt)
+        self.sprite.update(dt, self.position_vector())
     
     def draw(self, screen):
         self.sprite.draw(screen)   
+
+class Player(Character):
+    def __init__(self, sprite_details=('images\\player\\character\\', 4, 12), pos=(0, 0), lim=(0, DISPLAY_HEIGHT), hb=(20, 20), hp=5, s=200, cdt=0.5):
+        super().__init__(sprite_details, pos, lim, hb, hp, s, cdt)
+
+        self.powerup_effect_duration = 0.0
+        self.active_powerup = 0
+
+        self.powerup_active_speed = self.speed * 2
+        self.powerup_active_shoot_cooldown_time = self.shoot_cooldown_time * 0.5
+
+        self.hitpoints_bar = MeterBar(pos=(30, 50), dims=(200, 20), sm=0, col='Blue', amt=self.hitpoints)
+    
+    def _configure_self_from_powerups(self):
+        current_speed = self.speed
+        current_shoot_cooldown_time = self.shoot_cooldown_time
+
+        if self.powerup_effect_duration > 0:
+            if self.active_powerup == 1:
+                self.hitpoints += 1
+                self.powerup_effect_duration = 0
+                self.active_powerup = 0
+            elif self.active_powerup == 2:
+                current_speed = self.powerup_active_speed
+            elif self.active_powerup == 3:
+                current_shoot_cooldown_time = self.powerup_active_shoot_cooldown_time                
+        else:
+            self.active_powerup = 0
+        
+        return (current_speed, current_shoot_cooldown_time)
+    
+    def can_shoot(self):
+        current_speed, current_shoot_cooldown_time = self._configure_self_from_powerups()
+        if self.time_since_last_shoot >= current_shoot_cooldown_time:
+            return True
+        else:
+            return False
+
+    def update(self, dt=0, direction=0):
+        if self.active_powerup != 0:
+            self.powerup_effect_duration -= dt
+        
+        current_speed, current_shoot_cooldown_time = self._configure_self_from_powerups()
+        
+        self._manage_shoot_cooldown(dt, current_shoot_cooldown_time)
+        self._manage_excessive_hitpoints()
+        
+        projected_hitbox_rect = self._calculate_projected_position(dt, direction, current_speed)
+        if not projected_hitbox_rect.top <= self.y_range[0] and not projected_hitbox_rect.bottom >= self.y_range[1]:
+            self._update_hitbox_rect_position(projected_hitbox_rect)
+        
+        self.sprite.update(dt, self.position_vector())
+        self.hitpoints_bar.update(self.hitpoints)
+    
+    def draw(self, screen):
+        super().draw(screen)
+        self.hitpoints_bar.draw(screen)
+
+class GiantKillerSpaceRobot(Character):
+    def __init__(self, sprite_details=('images\\gksr\\phase1\\character\\', 4, 12), pos=(0, 0), lim=(0, DISPLAY_HEIGHT), hb=(20, 20), hp=100, s=0, cdt=2.0, ppw=4, po=10):
+        super().__init__(sprite_details, pos, lim, hb, hp, s, cdt)
+        self.attack_phase = 1
+        self.projectiles_per_wave = ppw
+
+        self.projectile_origin_positions = []
+        firing_range = lim
+        origin_amounts = po
+        for x in range(origin_amounts):
+            self.projectile_origin_positions.append((900, firing_range[0] + (((firing_range[1] - firing_range[0]) / origin_amounts) * (x + 1))))
+        
+        self.hitpoints_bar = MeterBar(pos=(1230, 50), dims=(200, 20), sm=1, col='Red', amt=self.hitpoints)
+    
+    def _manage_attack_phase(self):
+        health_percent = self.hitpoints / self.max_hitpoints
+        phase_changed = False
+        if health_percent < 0.66 and self.attack_phase == 1:
+            self.attack_phase = 2
+            phase_changed = True
+        elif health_percent < 0.33 and self.attack_phase == 2:
+            self.attack_phase = 3
+            phase_changed = True
+        
+        if phase_changed == True:
+            self.sprite = AnimatedSprite(f'images\\gksr\\phase{self.attack_phase}\\character\\', (self.pos_x, self.pos_y), 4, 12)
+    
+    def _manage_self_from_attack_phase(self):
+        if self.attack_phase == 1:
+            self.projectiles_per_wave = 4
+            self.cooldown_time = 2.0
+        elif self.attack_phase == 2:
+            self.projectiles_per_wave = 5
+            self.cooldown_time = 1.5
+        if self.attack_phase == 3:
+            self.projectiles_per_wave = 6
+            self.cooldown_time = 1.0
+
+    def update(self, dt=0):
+        super().update(dt, 0)
+
+        self._manage_attack_phase()
+        self._manage_self_from_attack_phase()
+
+        self.sprite.update(dt, self.position_vector())
+        self.hitpoints_bar.update(self.hitpoints)
+        return
+    
+    def draw(self, screen):
+        super().draw(screen)
+        self.hitpoints_bar.draw(screen)
 
 class GameObject():
     def __init__(self):
@@ -258,7 +377,7 @@ class PowerUp(GameObject):
     def check_hit(self, rect):
         return self.hitbox_rect.colliderect(rect)
 
-class Player(GameObject):
+class OldPlayer(GameObject):
     def __init__(self, path='test_images\\test_player.png', pos=(100, 360), lim=(0, DISPLAY_HEIGHT), hb=(20, 20), hp=5, s=200, cdt=0.5):
         super().__init__()
 
@@ -333,7 +452,7 @@ class Player(GameObject):
         super().draw(screen)
         self.hitpoints_bar.draw(screen)
 
-class GiantKillerSpaceRobot(GameObject):
+class OldGiantKillerSpaceRobot(GameObject):
     def __init__(self, path='test_images\\test_gksr_phase1.png', pos=(880, 170), hb=(300, 600), hp=100, s=200, cdt=2, init_ppw=6, mp=10, y_range=(0, DISPLAY_HEIGHT)):
         super().__init__()
 
@@ -427,10 +546,10 @@ def main():
     clock = pygame.time.Clock()
     delta_time = 0
 
-    player = Player(lim=(280, 580))
+    player = OldPlayer(lim=(280, 580))
     player_projectiles = []
 
-    gksr = GiantKillerSpaceRobot(y_range=(280, 550))
+    gksr = OldGiantKillerSpaceRobot(y_range=(280, 550))
     gksr_projectiles = []
 
     time_left = 60.0
@@ -446,7 +565,7 @@ def main():
     victory_card = StaticImage(path='test_images\\victory_card.png')
     ui_background_bar = StaticImage()
 
-    test_character = Character(sprite_details=('images\\gksr\\phase1\\character\\', 4, 12), pos=(300, 400), lim=(200, 600), hb=(20, 20), hp=10)
+    #test_player = GiantKillerSpaceRobot(pos=(300, 400))
 
     running = True
     while running:
@@ -459,25 +578,18 @@ def main():
             running = False
         
         if game_phase == 1:
-            # display the title card
-            # proceed to next game phase when player presses the space bar
-            # increase gamephase to 2
-            
-            test_character.update(delta_time, 0)
+            #test_player.update(delta_time)
 
             title_card.draw(screen)
-            test_character.draw(screen)
+            #test_player.draw(screen)
 
             if keys[pygame.K_SPACE]:
                 game_phase += 1
         elif game_phase == 2:
-            # initialize player and player projectiles
-            # initialize GKSR and gksr projectiles
-            # increase gamephase to 3
-            player = Player(lim=(280, 580), hp=5)
+            player = Player(pos=(100, 420), lim=(280, 580), hp=5)
             player_projectiles = []
 
-            gksr = GiantKillerSpaceRobot(y_range=(280, 550))
+            gksr = GiantKillerSpaceRobot(pos=(800, 420), lim=(280, 580), hb=(200, 500))
             gksr_projectiles = []
 
             powerup_pickups = []
@@ -489,16 +601,6 @@ def main():
 
             game_phase +=1
         elif game_phase == 3:
-            # run the game:
-            # update and draw the player and player projectiles
-            # update and draw the gksr and gksr projectiles
-            # ^^^ do this until:
-            # 1. the player's health is depleted
-            # 2. the gksr's health is depleted
-            # 3. the timer runs out
-            # ^^^ when this happens:
-            # change the value of game_end_scenario to 1, 2, or 3 depending on which of the above happened
-            # increase the gamephase to 4
             time_bar.update(time_left)
 
             if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
@@ -508,12 +610,12 @@ def main():
             else:
                 player.update(delta_time, 0)
             
-            if keys[pygame.K_SPACE] and player.shoot_cooldown >= player.current_cooldown_time:
+            if keys[pygame.K_SPACE] and player.can_shoot():
                 player_projectiles.append(Projectile(pos=player.hitbox_rect.center, dir=1))
-                player.shoot_cooldown = 0
+                player.time_since_last_shoot = 0
         
             gksr.update(delta_time)
-            if gksr.shoot_cooldown >= gksr.cooldown_time:
+            if gksr.can_shoot():
                 positions = choose_gksr_projectiles_origins(gksr)
                 for position in positions:
                     gksr_projectiles.append(Projectile(path='test_images\\test_gskr_projectile.png', pos=position, hb=(20,32), s=500, dir=-1))
@@ -523,7 +625,7 @@ def main():
                 if random_num < chance:
                     powerup_pickups.append(spawn_powerup())
 
-                gksr.shoot_cooldown = 0
+                gksr.time_since_last_shoot = 0
 
             player_projectiles = update_projectile_group(player_projectiles, delta_time, gksr)
             gksr_projectiles = update_projectile_group(gksr_projectiles, delta_time, player)
