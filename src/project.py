236b633648ccb95d81a05974d16f7cdb60b4ccbd
Ivan Vazquez <Ivanvqz404@gmail.com>
2025-04-20 04:10:03 -0500

    Extracted updating of player based on keys pressed to function update_player_from_keys().

--- src/project.py
+++ src/project.py
@@ -384,206 +384,16 @@ class MeterBar():
         pygame.draw.rect(screen, pygame.Color(self.color).lerp('Black', 0.5), self.background_rect)
         pygame.draw.rect(screen, self.color, self.draw_rect)
 
-class OldProjectile(GameObject):
-    def __init__(self, path='test_images\\test_projectile.png', pos=(0, 0), hb=(8, 8), s=750, dir=0, dmg=1):
-        super().__init__()
-        self.pos_x, self.pos_y = pos
-        
-        self.image = pygame.image.load(path).convert_alpha()
-        self.draw_rect = self.image.get_rect()
-        self.draw_rect.center = (self.pos_x, self.pos_y)
-
-        self.hitbox_rect = pygame.Rect((0, 0), hb)
-        self.hitbox_rect.center = (self.pos_x, self.pos_y)
-
-        self.speed = s
-        self.direction = dir
-        self.onscreen = True
-        self.damage = dmg
-
-    def _is_onscreen(self):
-        game_window_rect = pygame.Rect((0,0), (DISPLAY_WIDTH, DISPLAY_HEIGHT))
-        return game_window_rect.colliderect(self.draw_rect)
+def update_player_from_keys(player, keys, dt=0):
+    direction = None
+    if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
+        direction = -1
+    elif keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
+        direction = 1
+    else:
+        direction = 0
     
-    def update(self, dt=0):
-        super().update()
-        
-        self.onscreen = self._is_onscreen()
-        if self.onscreen:
-            self.pos_x += (self.speed * self.direction) * dt
-            self.draw_rect.center = (self.pos_x, self.pos_y)
-            self.hitbox_rect.center = (self.pos_x, self.pos_y)
-    
-    def check_hit(self, rect):
-        return self.hitbox_rect.colliderect(rect)
-
-class OldPowerUp(GameObject):
-    def __init__(self, path='test_images\\test_powerup.png', pos=(900, 360), hb=(20, 20), code=1):
-        super().__init__()
-        self.pos_x, self.pos_y = pos
-        
-        self.image = pygame.image.load(path).convert_alpha()
-        self.draw_rect = self.image.get_rect()
-        self.draw_rect.center = (self.pos_x, self.pos_y)
-
-        self.hitbox_rect = pygame.Rect((0, 0), hb)
-        self.hitbox_rect.center = (self.pos_x, self.pos_y)
-
-        self.powerup_code = code
-        self.powerup_duration = 5.0
-        self.direction = -1
-        self.speed = 350
-        self.onscreen = True
-    
-    def _is_onscreen(self):
-        game_window_rect = pygame.Rect((0,0), (DISPLAY_WIDTH, DISPLAY_HEIGHT))
-        return game_window_rect.colliderect(self.draw_rect)
-    
-    def update(self, dt=0):
-        super().update()
-        
-        self.onscreen = self._is_onscreen()
-        if self.onscreen:
-            self.pos_x += (self.speed * self.direction) * dt
-            self.draw_rect.center = (self.pos_x, self.pos_y)
-            self.hitbox_rect.center = (self.pos_x, self.pos_y)
-    
-    def check_hit(self, rect):
-        return self.hitbox_rect.colliderect(rect)
-
-class OldPlayer(GameObject):
-    def __init__(self, path='test_images\\test_player.png', pos=(100, 360), lim=(0, DISPLAY_HEIGHT), hb=(20, 20), hp=5, s=200, cdt=0.5):
-        super().__init__()
-
-        self.pos_x, self.pos_y = pos
-        
-        self.image = pygame.image.load(path).convert_alpha()
-        self.draw_rect = self.image.get_rect()
-        self.draw_rect.center = (self.pos_x, self.pos_y)
-
-        self.min_y = lim[0] + (self.draw_rect.height / 2)
-        self.max_y = lim[1] - (self.draw_rect.height / 2)
-
-        self.hitbox_rect = pygame.Rect((0, 0), hb)
-        self.hitbox_rect.center = (self.pos_x, self.pos_y)
-
-        self.hitpoints = hp
-        self.max_hitpoints = hp
-
-        self.speed = s
-
-        self.shoot_cooldown = 0
-        self.cooldown_time = cdt
-        self.current_cooldown_time = cdt
-
-        self.powerup_effect_duration = 0.0
-        self.active_powerup = 0
-
-        self.speed_when_powerup = self.speed * 2
-        self.cooldown_time_when_powerup = self.cooldown_time * 0.5
-
-        self.hitpoints_bar = MeterBar(pos=(30, 50), dims=(200, 20), sm=0, col='Blue', amt=self.hitpoints)
-
-    def update(self, dt=0, direction=0):
-        super().update()
-
-        if self.active_powerup != 0:
-            self.powerup_effect_duration -= dt
-        
-        current_speed = self.speed
-        self.current_cooldown_time = self.cooldown_time
-
-        if self.powerup_effect_duration > 0:
-            if self.active_powerup == 1:
-                self.hitpoints += 1
-                self.powerup_effect_duration = 0
-                self.active_powerup = 0
-            elif self.active_powerup == 2:
-                current_speed = self.speed_when_powerup
-            elif self.active_powerup == 3:
-                self.current_cooldown_time = self.cooldown_time_when_powerup                
-        else:
-            self.active_powerup = 0
-
-        if self.hitpoints > self.max_hitpoints:
-            self.hitpoints = self.max_hitpoints
-        
-        projected_y = self.pos_y + ((current_speed * direction) * dt)
-        projected_draw_rect = self.draw_rect.copy()
-        projected_draw_rect.center = (self.pos_x, projected_y)
-
-        if not projected_draw_rect.top <= self.min_y and not projected_draw_rect.bottom >= self.max_y:
-            self.pos_y = projected_y
-            self.draw_rect.center = (self.pos_x, self.pos_y)
-            self.hitbox_rect.center = (self.pos_x, self.pos_y)
-
-        if self.shoot_cooldown <= self.current_cooldown_time:
-            self.shoot_cooldown += dt
-        
-        self.hitpoints_bar.update(self.hitpoints)
-
-    def draw(self, screen):
-        super().draw(screen)
-        self.hitpoints_bar.draw(screen)
-
-class OldGiantKillerSpaceRobot(GameObject):
-    def __init__(self, path='test_images\\test_gksr_phase1.png', pos=(880, 170), hb=(300, 600), hp=100, s=200, cdt=2, init_ppw=6, mp=10, y_range=(0, DISPLAY_HEIGHT)):
-        super().__init__()
-
-        self.pos_x, self.pos_y = pos
-        
-        self.image = pygame.image.load(path).convert_alpha()
-        self.draw_rect = self.image.get_rect()
-        self.draw_rect.topleft = (self.pos_x, self.pos_y)
-
-        self.hitbox_rect = pygame.Rect((980, 120), hb)
-
-        self.hitpoints = hp
-        self.max_hitpoints = hp
-        self.attack_phase = 1
-        self.speed = s
-        self.shoot_cooldown = 0
-        self.cooldown_time = cdt
-        self.projectiles_per_wave = init_ppw
-
-        self.projectile_origin_positions = []
-        firing_range = y_range
-        origin_amounts = mp
-        for x in range(origin_amounts):
-            self.projectile_origin_positions.append((900, firing_range[0] + (((firing_range[1] - firing_range[0]) / origin_amounts) * (x + 1))))
-        
-        self.hitpoints_bar = MeterBar(pos=(1230, 50), dims=(200, 20), sm=1, col='Red', amt=self.hitpoints)
-    
-    def update(self, dt=0):
-        super().update()
-        
-        if self.shoot_cooldown <= self.cooldown_time:
-            self.shoot_cooldown += dt
-        
-        health_percent = self.hitpoints / self.max_hitpoints
-        if health_percent < 0.66 and self.attack_phase == 1:
-            self.attack_phase = 2
-        elif health_percent < 0.33 and self.attack_phase == 2:
-            self.attack_phase = 3
-
-        if self.attack_phase == 1:
-            self.image = pygame.image.load('test_images\\test_gksr_phase1.png').convert_alpha()
-            self.projectiles_per_wave = 4
-            self.cooldown_time = 2.0
-        elif self.attack_phase == 2:
-            self.image = pygame.image.load('test_images\\test_gksr_phase2.png').convert_alpha()
-            self.projectiles_per_wave = 5
-            self.cooldown_time = 1.5
-        if self.attack_phase == 3:
-            self.image = pygame.image.load('test_images\\test_gksr_phase3.png').convert_alpha()
-            self.projectiles_per_wave = 6
-            self.cooldown_time = 1.0
-
-        self.hitpoints_bar.update(self.hitpoints)
-        
-    def draw(self, screen):
-        super().draw(screen)
-        self.hitpoints_bar.draw(screen)
+    player.update(dt, direction)
 
 def choose_gksr_projectiles_origins(gksr):
     positions = []
@@ -593,7 +403,7 @@ def choose_gksr_projectiles_origins(gksr):
             positions.append(pos)
     return positions
 
-def update_projectile_group(projectiles, delta_time, target):
+def update_group(projectiles, delta_time, target):
     new_projectiles = []
     for projectile in projectiles:
         if projectile.onscreen:
@@ -620,12 +430,13 @@ def main():
     clock = pygame.time.Clock()
     delta_time = 0
 
-    player = OldPlayer(lim=(280, 580))
+    player = Player(pos=(100, 420), lim=(230, 640), hp=5)
     player_projectiles = []
 
-    gksr = OldGiantKillerSpaceRobot(y_range=(280, 550))
+    gksr = GiantKillerSpaceRobot(pos=(1050, 420), lim=(180, 680), hb=(200, 500))
     gksr_projectiles = []
 
+    powerup_pickups = []
     blasts = []
 
     time_left = 60.0
@@ -669,6 +480,7 @@ def main():
             gksr_projectiles = []
 
             powerup_pickups = []
+            blasts = []
 
             game_end_scenario = 0
             
@@ -679,12 +491,7 @@ def main():
         elif game_phase == 3:
             time_bar.update(time_left)
 
-            if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
-                player.update(delta_time, -1)
-            elif keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
-                player.update(delta_time, 1)
-            else:
-                player.update(delta_time, 0)
+            update_player_from_keys(player, keys, delta_time)
             
             if keys[pygame.K_SPACE] and player.can_shoot():
                 player_projectiles.append(Projectile(('images\\player\\projectile\\', 3, 12), player.position_vector(), s=500, dir=1, dmg=1))
@@ -703,8 +510,8 @@ def main():
 
                 gksr.time_since_last_shoot = 0
 
-            player_projectiles = update_projectile_group(player_projectiles, delta_time, gksr)
-            gksr_projectiles = update_projectile_group(gksr_projectiles, delta_time, player)
+            player_projectiles = update_group(player_projectiles, delta_time, gksr)
+            gksr_projectiles = update_group(gksr_projectiles, delta_time, player)
 
             new_powerup_pickups = []
             for powerup in powerup_pickups:
